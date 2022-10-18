9924c5ac159d1f11956dee9322f81bf26db41cad
Eduardo Sandoval <eduardosandoval.atc@gmail.com>
2022-10-17 21:49:33 -0500

    desktop / mobile Menu

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,35 +1,47 @@
-import React from 'react'
-import {Link} from "react-router-dom";
+import React, { useState } from 'react';
+import { Link } from "react-router-dom";
 import "./Navbar.css";
 
-const Navbar = ({children}) => {
-  return (
-    <>
-    <nav className='navbar'>
-        <h3 className='navbar__logo'>sandov</h3>
-        <ul className='navbar__links'>
-            <Link to="/" className='home'>
-                <li>Home</li>
-            </Link>
-            <Link to="/about" className='about'>
-                <li>About</li>
-            </Link>
-            <Link to="/skills" className='skills'>
-                <li>Skills</li>
-            </Link>
-            <Link to="/contact" className='contact'>
-                <li>Contact</li>
-            </Link>
-            <Link to="/signup" className='signup'>
-                <li>Sign Up</li>
-            </Link>
+const Navbar = ({ children }) => {
+    const [isMobile, setIsMobile] = useState(false);
+    return (
+        <>
+            <nav className='navbar'>
+                <h3 className='navbar__logo'>sandov</h3>
+                <ul className= {isMobile ? "navbar__links-mobile" : "navbar__links"}
+                onClick={() => setIsMobile(false)}
+                >
+                    <Link to="/" className='home'>
+                        <li>Home</li>
+                    </Link>
+                    <Link to="/about" className='about'>
+                        <li>About</li>
+                    </Link>
+                    <Link to="/skills" className='skills'>
+                        <li>Skills</li>
+                    </Link>
+                    <Link to="/contact" className='contact'>
+                        <li>Contact</li>
+                    </Link>
+                    <Link to="/signup" className='signup'>
+                        <li>Sign Up</li>
+                    </Link>
 
-        </ul>
-        
-    </nav>
-    {children}
-    </>
-  )
+                </ul>
+                <button className="mobile-menu-icon"
+                    onClick={() => setIsMobile(!isMobile)}
+                >
+                    {isMobile ? ( 
+                    <i className="fas fa-times"></i>
+                    ) : (
+                        <i className="fas fa-bars"></i>
+                    )}
+                </button>
+
+            </nav>
+            {children}
+        </>
+    )
 }
 
 export default Navbar
